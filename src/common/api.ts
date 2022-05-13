e0c663bcb16eddbae5663185bcf912b7a81e9660
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-05-13 08:05:49 +0300

    Add getFolders request to api and getToken

--- src/common/api.ts
+++ src/common/api.ts
@@ -4,11 +4,20 @@ const axios = require('axios').default;
 class Api {
     serverUrl: string = 'http://localhost:3000'
 
+    getToken() { // TODO refactor
+        return sessionStorage.getItem('token')
+    }
+
     login (email: string, password: string) {
         return axios.post(`${this.serverUrl}/api/login`, {
             email,
             password
         })
+    }
+
+    getFolders() {
+        return axios.get( `${this.serverUrl}/api/get-folders` ,
+            { headers: {"Authorization" : this.getToken()} })
     }
 }
 
