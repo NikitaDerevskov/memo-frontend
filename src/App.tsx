4915d772d3f0a549c1ed32be4fc9f06f937d0742
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-05-14 23:28:33 +0300

    Add non match and folder routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import { BrowserRouter,  Routes, Route } from "react-router-dom";
 import Folders from "./features/folders/Folders";
-import {useState} from "react";
 import Welcome from "./features/auth/Welcome";
 import Login from "./features/auth/Login";
 import Registration from "./features/auth/Registration";
+import Folder from "./features/folders/Folder";
 
 function App() {
-  const [folders, setFolders] = useState([]);
-  // TODO fetch folders list
-
   return (
       <BrowserRouter>
           <Routes>
               <Route path="/" element={<Welcome />} />
-              <Route path="main" element={<Folders />} />
               <Route path="login" element={<Login />} />
               <Route path="registration" element={<Registration />} />
+              <Route path="main" element={<Folders />} />
+              <Route path="folder" element={<Folder />}>
+                  <Route path=":ifolderId" element={<Folder />} />
+              </Route>
+              <Route
+                  path="*"
+                  element={
+                      <main style={{ padding: "1rem" }}>
+                          <p>There's nothing here!</p>
+                      </main>
+                  }
+              />
           </Routes>
       </BrowserRouter>
-    // <div className="App">
-    //     <Welcome />
-    //     <Folders />
-    // </div>
   );
 }
 
