787fb25f21b0ec84465c91a31a64d9125ef1372c
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-06-23 23:58:08 +0300

    Create structure to Header component

---
+++ src/features/header/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <div>
+        <div className="header-page-name">Page name</div>
+        <div className="header-tools">
+            <div className="header-user-avatar"></div>
+            <div className="header-user-name"></div>
+            <div className="header-settings"></div>
+        </div>
+    </div>
+}
+
+export default Header
