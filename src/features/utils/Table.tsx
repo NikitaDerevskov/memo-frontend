d89d074a9478923d3e0290fa6349ef8264907789
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-06-29 21:20:10 +0300

    Add todos and basic structure to Table component

--- src/features/utils/Table.tsx
+++ src/features/utils/Table.tsx
@@ -1,7 +1,16 @@
-function Table() {
-    return <div>
-        {'Table'}
-    </div>
+function Table(props: { children: any }) { /* TODO change type to good */
+    return (
+        <>
+            { props.children }
+            {/* TODO don't show pagination if count <= limit */}
+            <div className="pagination">
+                {/* TODO use good function like in hooks tutorial :) */}
+                {/* TODO add disabled when needed */}
+                <button>{'<'}</button>
+                {'0 from 5'}
+                <button>{'>'}</button>
+            </div>
+        </>)
 }
 
 export default Table
