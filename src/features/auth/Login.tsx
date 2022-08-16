fb391d19d2422623d103f6d336a06d4545ec03c6
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-08-16 22:31:22 +0300

    login: add markup

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -3,6 +3,7 @@ import {
 } from 'react-router-dom';
 import { useState } from 'react';
 import Api from '../../common/api';
+import { PrimaryButton } from '../utils/PrimaryButton';
 
 function Login() {
   const navigate = useNavigate();
@@ -11,39 +12,46 @@ function Login() {
   const [password, setPassword] = useState('');
 
   return (
-    <div className="login">
-      Login
+    <main className="container h-screen flex items-center">
+      <div className="container space-y-4">
+        <h1 className="text-center">Login</h1>
 
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button onClick={() => {
-        // TODO add error handling
-        Api.login(email, password)
-          .then(({ data }: { data: string }) => {
-            /* TODO work with Bearer */
-            /* TODO change to redux - just for fun */
-            sessionStorage.setItem('token', `Bearer ${data}`);
-            navigate('/main');
-          })
-          .catch((e: any) => {
-            alert(e.response.data);
-          });
-      }}
-      >
-        Sign in
-      </button>
-    </div>
+        <div className="flex flex-col items-center space-y-4">
+          <div className="flex flex-col space-y-4">
+            <label htmlFor='email'>Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+             />
+          </div>
+          <div className="flex flex-col space-y-4">
+            <label htmlFor='password'>Password</label>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <PrimaryButton text="Sign in" onClick={() => {
+            Api.login(email, password)
+              .then(({ data }: { data: string }) => {
+                /* TODO work with Bearer */
+                /* TODO change to redux - just for fun */
+                sessionStorage.setItem('token', `Bearer ${data}`);
+                navigate('/main');
+              })
+              .catch((e: any) => {
+                alert(e.response.data);
+              });
+          }}/>
+        </div>
+      </div>
+    </main>
   );
 }
 
