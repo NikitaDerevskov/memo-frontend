31044f3c3881c24fc900795d2b2a203043f3fa8e
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-08-16 23:06:16 +0300

    header: use settings icon and improve styles

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,16 +1,23 @@
+import settingsLogo from '../../common/img/icons/settings.svg';
+
 export default function Header({ pageName } : { pageName: string }) {
 
   /* TODO refactor to redux */
   let userName = sessionStorage.getItem('userName');
 
+  let handleSettingsClick = () => {
+    /* TODO */
+    console.log('Navigate to settings');
+  };
 
   return (
     <header className="flex justify-between border-b-2 p-3">
       <div className="header-page-name">{pageName}</div>
-      <div className="header-tools">
-        <div className="header-user-avatar" />
+      <div className="header-tools flex space-x-4">
         <div className="header-user-name">{userName}</div>
-        <div className="header-settings" />
+        <div className="header-settings mt-1 cursor-pointer" onClick={handleSettingsClick}>
+          <img src={settingsLogo} alt='settings' />
+        </div>
       </div>
     </header>
   );
