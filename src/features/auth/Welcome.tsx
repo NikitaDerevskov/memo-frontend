77c55d34d01290954616af4aef7cb884c68ef460
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-08-16 22:10:53 +0300

    welcome: update markup

--- src/features/auth/Welcome.tsx
+++ src/features/auth/Welcome.tsx
@@ -1,14 +1,15 @@
-import { Link } from 'react-router-dom';
+import { PrimaryLink } from '../utils/PrimaryLink';
+import { SecondaryLink } from '../utils/SecondaryLink';
 
 function Welcome() {
   return (
     <main className="container h-screen flex align-middle items-center">
       <div className="container flex flex-col align-middle max-w-lg -mt-24">
         <h1 className="text-center">Welcome to Memo</h1>
-        <div className="links flex justify-end space-x-4">
+        <div className="links flex justify-end space-x-4 mt-5">
           {/* TODO think about orger sign up and sign in  */}
-          <Link to="registration">Sign up</Link>
-          <Link to="login">Sing in</Link>
+          <SecondaryLink to="registration" text="Sign up" />
+          <PrimaryLink to="login" text="Sing in"/>
         </div>
       </div>
     </main>
