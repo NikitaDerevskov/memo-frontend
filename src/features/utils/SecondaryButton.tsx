da2acdc9b80f642c58e1f554db72f2bf7ed8d6c4
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-08-23 22:51:39 +0300

    utils: SecondaryButton.tsx: make className optional param

--- src/features/utils/SecondaryButton.tsx
+++ src/features/utils/SecondaryButton.tsx
@@ -1,4 +1,4 @@
-export default function secondaryButton({ onClick, text, className }: { onClick: () => void, text: string, className: string }) {
+export default function secondaryButton({ onClick, text, className }: { onClick: () => void, text: string, className?: string }) {
   return (
       <button onClick={onClick} className={`bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded ${className}`}>{text}</button>);
 }
