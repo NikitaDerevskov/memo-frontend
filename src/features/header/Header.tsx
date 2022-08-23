42d11012352577b6b4cfee7004251ee8dd8f87f4
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-08-23 22:22:24 +0300

    icons: add back icon; header: add back button logic

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,7 +1,9 @@
 import settingsLogo from '../../common/img/icons/settings.svg';
+import { useNavigate } from 'react-router-dom';
+import backIcon from '../../common/img/icons/back.svg';
 
-export default function Header({ pageName } : { pageName: string }) {
-
+export default function Header({ pageName, backRoute } : { pageName: string, backRoute?: string }) {
+  const navigate = useNavigate();
   /* TODO refactor to redux */
   let userName = sessionStorage.getItem('userName');
 
@@ -10,9 +12,19 @@ export default function Header({ pageName } : { pageName: string }) {
     console.log('Navigate to settings');
   };
 
+  let handleBackClick = () => {
+    if (backRoute) {
+      navigate(backRoute);
+    }
+  };
+
   return (
     <header className="flex justify-between border-b-2 p-3">
-      <div className="header-page-name">{pageName}</div>
+      <div className="flex space-x-4">
+        {/* TODO add component for icon */}
+        {backRoute && <img className='header-back w-4 mx-auto cursor-pointer' src={backIcon} onClick={handleBackClick} alt="back"/>}
+        <div className="header-page-name">{pageName}</div>
+      </div>
       <div className="header-tools flex space-x-4">
         <div className="header-user-name">{userName}</div>
         <div className="header-settings mt-1 cursor-pointer" onClick={handleSettingsClick}>
