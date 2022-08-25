7cc2cc9c92b0f9670b4ab2b209916987fee501cd
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-08-25 21:42:38 +0300

    cards: create CardModal.tsx

---
+++ src/features/cards/CardModal.tsx
@@ -0,0 +1,52 @@
+import Modal from '../utils/Modal';
+import { CardT } from '../../common/types';
+import { PrimaryButton } from '../utils/PrimaryButton';
+import SecondaryButton from '../utils/SecondaryButton';
+
+/* TODO add teleport for modal */
+
+export function CardModal({
+  cardIndex,
+  currentCard,
+  showContent,
+  showContentHandler,
+  cardsCount,
+  nextHandler,
+  prevHandler,
+  closeHandler }: {
+  cardIndex: number,
+  currentCard: CardT,
+  showContent: boolean,
+  showContentHandler: () => void,
+  cardsCount: number,
+  nextHandler: () => void,
+  prevHandler: () => void,
+  closeHandler: () => void }) {
+
+
+  const { title, content } = currentCard;
+
+  /* TODO work with pagination better */
+  return (
+    <>
+      <Modal>
+        <div>
+          { cardIndex + 1}
+          /
+          { cardsCount }
+        </div>
+        <div>
+          {title}
+          {' '}
+          {showContent && content}
+        </div>
+        <PrimaryButton text='Show' onClick={showContentHandler} />
+        <div className='pagination'>
+          <SecondaryButton text="<" onClick={prevHandler}/>
+          <SecondaryButton text=">" onClick={nextHandler}/>
+        </div>
+        <SecondaryButton text="Close" onClick={closeHandler}/>
+      </Modal>
+    </>
+  );
+}
