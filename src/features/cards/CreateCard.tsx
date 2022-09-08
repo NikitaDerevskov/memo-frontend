11808d0fe99d806e445baa47a46e8e5d28691036
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-09-08 23:41:46 +0300

    cards: CreateCard: refactor to Formik

--- src/features/cards/CreateCard.tsx
+++ src/features/cards/CreateCard.tsx
@@ -1,107 +1,98 @@
-import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import Modal from "../utils/Modal";
 import Api from "../../common/api";
 import Header from "../header/Header";
 import { PrimaryButton } from "../utils/PrimaryButton";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Input } from "../utils/Input";
 
 /* TODO add validation */
 /* TODO refactor work to state from url it will be better */
-function CreateCard({ title, content, last_modified }: any) {
+function CreateCard() {
   /* TODO fix type */
-  const isCardExisted = Boolean(last_modified);
   const location = useLocation();
   const { id } = location.state as any;
 
   const navigate = useNavigate();
 
-  /* TODO add work with loading and error */
-  const [isLoading, setIsLoading] = useState(false);
-  console.log("isLoading", isLoading);
-  const [err, setError] = useState("");
-  console.log("err", err);
-  const [cardData, setCardData] = useState({ title, content, last_modified });
-
-  const handleClick = async () => {
-    try {
-      /* TODO think about last_modified - now this is undefined */
-      const response = await Api.createCard({ folderId: id, ...cardData });
-
-      navigate(`/folder/${id}`, { state: { id } });
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-    } catch (err: any) {
-      /* TODO fix type */
-      setError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const newCardJsx = (
+  return (
     <>
       <Header
         backRoute={{ to: `/folder/${id}`, options: { state: { id } } }}
         pageName="New card"
       />
       <main className="container h-screen flex flex-col align-middle items-center">
-        <div className="create-card-title flex flex-col space-y-4 mt-4">
-          <label htmlFor="title">Title</label>
-          <input
-            id="title"
-            placeholder="Cuber Punk"
-            type="text"
-            onChange={(event) => {
-              setCardData({ ...cardData, title: event.target.value });
-            }}
-          />
-        </div>
-        <div className="create-card-content flex flex-col space-y-4 mt-4">
-          <label htmlFor="content">Content</label>
-          <textarea
-            id="content"
-            placeholder="V going to change the world"
-            onChange={(event) => {
-              setCardData({ ...cardData, content: event.target.value });
-            }}
-          />
-        </div>
-        <PrimaryButton
-          className="max-w-xs mt-4 mb-4"
-          text="Create"
-          onClick={handleClick}
-        />
+        <Formik
+          initialValues={{ title: "", content: "" }}
+          validationSchema={Yup.object().shape({
+            title: Yup.string().required("Required"),
+            content: Yup.string().required("Required"),
+          })}
+          onSubmit={(values) => {
+            Api.createCard({
+              folderId: id,
+              title: values.title,
+              content: values.content,
+            })
+              .then(() => {
+                navigate(`/folder/${id}`, { state: { id } });
+              })
+              .catch((e: any) => alert(e));
+          }}
+        >
+          {(props) => {
+            const {
+              values,
+              touched,
+              errors,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            } = props;
+            return (
+              <form onSubmit={handleSubmit}>
+                <div className="create-card-title flex flex-col space-y-4 mt-4">
+                  <label htmlFor="title">Title</label>
+                  <Input
+                    id="title"
+                    placeholder="Cuber Punk"
+                    type="text"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.title && touched.title && (
+                    <div className="input-feedback text-rose-600">
+                      {errors.title}
+                    </div>
+                  )}
+                </div>
+                <div className="create-card-content flex flex-col space-y-4 mt-4">
+                  <label htmlFor="content">Content</label>
+                  <textarea
+                    id="content"
+                    placeholder="V going to change the world"
+                    value={values.content}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.content && touched.content && (
+                    <div className="input-feedback text-rose-600">
+                      {errors.content}
+                    </div>
+                  )}
+                </div>
+                <PrimaryButton
+                  className="max-w-xs mt-4 mb-4"
+                  text="Create"
+                  type="submit"
+                />
+              </form>
+            );
+          }}
+        </Formik>
       </main>
     </>
   );
-
-  const existedCardJsx = (
-    <Modal>
-      <>
-        <div className="create-card-title">
-          <label htmlFor="title">Title</label>
-          <input
-            id="title"
-            placeholder="Cuber Punk"
-            type="text"
-            value={title}
-          />
-        </div>
-        <div className="create-card-content">
-          <label htmlFor="content">Content</label>
-          <textarea
-            id="content"
-            placeholder="V going to change the world"
-            value={content}
-          />
-        </div>
-        <div className="create-card-last_modified">{last_modified}</div>
-        <button className="create-card-edit">Edit</button>
-      </>
-    </Modal>
-  );
-  return isCardExisted ? existedCardJsx : newCardJsx;
 }
 
 export default CreateCard;
