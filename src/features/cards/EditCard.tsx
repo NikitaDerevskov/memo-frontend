f033188e1fbd9de41dbe3e8659eb6b8a161a98bc
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-09-08 23:50:14 +0300

    cards: EditCard: refactor to formik

--- src/features/cards/EditCard.tsx
+++ src/features/cards/EditCard.tsx
@@ -1,60 +1,22 @@
-import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Api from "../../common/api";
 import Header from "../header/Header";
 import { PrimaryButton } from "../utils/PrimaryButton";
 import { Input } from "../utils/Input";
 import { TextArea } from "../utils/TextArea";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 /* TODO add validation */
 /* TODO refactor work to state from url it will be better */
 function EditCard() {
   /* TODO fix type */
   const location = useLocation();
-  const {
-    folderId,
-    id,
-    title,
-    content,
-    last_modified: lastModified,
-  } = location.state as any;
+  const { folderId, id, title, content } = location.state as any;
 
   const navigate = useNavigate();
 
-  /* TODO add work with loading and error */
-  const [isLoading, setIsLoading] = useState(false);
-  console.log("isLoading", isLoading);
-  const [err, setError] = useState("");
-  console.log("err", err);
-  const [cardData, setCardData] = useState({
-    title,
-    content,
-    last_modified: lastModified,
-    id,
-  });
-
-  const handleClick = async () => {
-    try {
-      /* TODO think about last_modified - now this is undefined */
-      const response = await Api.editCard(
-        cardData.title,
-        cardData.content,
-        cardData.id
-      );
-
-      navigate(`/folder/${folderId}`, { state: { id: folderId } });
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-    } catch (err: any) {
-      /* TODO fix type */
-      setError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const editCardJsx = (
+  return (
     <>
       <Header
         backRoute={{
@@ -64,37 +26,74 @@ function EditCard() {
         pageName={`${title} card`}
       />
       <main className="container h-screen flex flex-col align-middle items-center">
-        <div className="create-card-title flex flex-col space-y-4 mt-4">
-          <label htmlFor="title">Title</label>
-          <Input
-            id="title"
-            type="text"
-            value={cardData.title}
-            onChange={(event) => {
-              setCardData({ ...cardData, title: event.target.value });
-            }}
-          />
-        </div>
-        <div className="create-card-content flex flex-col space-y-4 mt-4">
-          <label htmlFor="content">Content</label>
-          <TextArea
-            id="content"
-            value={cardData.content}
-            onChange={(event) => {
-              setCardData({ ...cardData, content: event.target.value });
-            }}
-          />
-        </div>
-        <PrimaryButton
-          className="max-w-xs mt-4 mb-4"
-          text="Edit"
-          onClick={handleClick}
-        />
+        <Formik
+          initialValues={{ title, content, id }}
+          validationSchema={Yup.object().shape({
+            title: Yup.string().required("Required"),
+            content: Yup.string().required("Required"),
+          })}
+          onSubmit={(values) => {
+            Api.editCard(values.title, values.content, values.id)
+              .then(() => {
+                navigate(`/folder/${folderId}`, { state: { id: folderId } });
+              })
+              .catch((e: any) => alert(e));
+          }}
+        >
+          {(props) => {
+            const {
+              values,
+              touched,
+              errors,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            } = props;
+            return (
+              <form onSubmit={handleSubmit}>
+                <div className="create-card-title flex flex-col space-y-4 mt-4">
+                  <label htmlFor="title">Title</label>
+                  <Input
+                    id="title"
+                    placeholder="Cuber Punk"
+                    type="text"
+                    value={values.title}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.title && touched.title && (
+                    <div className="input-feedback text-rose-600">
+                      {errors.title}
+                    </div>
+                  )}
+                </div>
+                <div className="create-card-content flex flex-col space-y-4 mt-4">
+                  <label htmlFor="content">Content</label>
+                  <TextArea
+                    id="content"
+                    placeholder="V going to change the world"
+                    value={values.content}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.content && touched.content && (
+                    <div className="input-feedback text-rose-600">
+                      {errors.content}
+                    </div>
+                  )}
+                </div>
+                <PrimaryButton
+                  className="max-w-xs mt-4 mb-4"
+                  text="Edit"
+                  type="submit"
+                />
+              </form>
+            );
+          }}
+        </Formik>
       </main>
     </>
   );
-
-  return editCardJsx;
 }
 
 export default EditCard;
