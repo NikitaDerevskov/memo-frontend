4728c94213a8eff86ef2c6f48fdae7badeb81406
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-09-08 23:19:16 +0300

    folders: refactor create folder to formik

--- src/features/folders/CreateFolder.tsx
+++ src/features/folders/CreateFolder.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Api from "../../common/api";
 import Header from "../header/Header";
 import { PrimaryButton } from "../utils/PrimaryButton";
 import { Input } from "../utils/Input";
+import { Formik } from "formik";
 
 /* TODO add validation */
 /* TODO refactor work to state from url it will be better */
@@ -11,56 +11,62 @@ function createFolder() {
   /* TODO fix type */
   const navigate = useNavigate();
 
-  /* TODO add work with loading and error */
-  const [isLoading, setIsLoading] = useState(false);
-  const [err, setError] = useState("");
-  console.log(isLoading, err);
-  const [folderTitle, setFolderTitle] = useState("");
-
-  const handleClick = async () => {
-    try {
-      const response = await Api.createFolder(folderTitle);
-      // TODO return from createfolder folder id and move to it.
-      navigate("/main");
-
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-    } catch (err: any) {
-      /* TODO fix type */
-      setError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   /* TODO extract basic structure of create and edit to 1 component */
   /* TODO add styles to all inputs (Maybe create 1 component for all) */
-  const createFolderJsx = (
+  return (
     <>
       <Header backRoute="/main" pageName="New Folder" />
       <main className="container h-screen flex flex-col align-middle items-center">
-        <div className="edit-folder-button flex flex-col space-y-4 mt-4">
-          <label htmlFor="title">Title</label>
-          <Input
-            id="title"
-            placeholder="Cyber Punk"
-            type="text"
-            onChange={(event) => {
-              setFolderTitle(event.target.value);
-            }}
-          />
-        </div>
-        <PrimaryButton
-          className="max-w-xs mt-4 mb-4"
-          text="Create"
-          onClick={handleClick}
-        />
+        <Formik
+          initialValues={{ title: "" }}
+          onSubmit={async (values) => {
+            // TODO add error handling
+            await Api.createFolder(values.title)
+              .then(() => {
+                navigate("/main");
+              })
+              .catch((e: any) => alert(e));
+          }}
+        >
+          {(props) => {
+            const {
+              values,
+              touched,
+              errors,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            } = props;
+            return (
+              <form onSubmit={handleSubmit}>
+                <div className="edit-folder-button flex flex-col space-y-4 mt-4">
+                  <label htmlFor="title">Title</label>
+                  <Input
+                    id="title"
+                    placeholder="Cyber Punk"
+                    type="text"
+                    value={values.title}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  {errors.title && touched.title && (
+                    <div className="input-feedback text-rose-600">
+                      {errors.title}
+                    </div>
+                  )}
+                </div>
+                <PrimaryButton
+                  className="max-w-xs mt-4 mb-4"
+                  text="Create"
+                  type="submit"
+                />
+              </form>
+            );
+          }}
+        </Formik>
       </main>
     </>
   );
-
-  return createFolderJsx;
 }
 
 export default createFolder;
