6bc1b06c8c8cc4f308ddf0259994ec6dd2adff50
Nikita Derevskov <n.derevskov@examus.com>
2022-11-08 23:27:30 +0300

    install and use maintine

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mantine/core';
 
 function App() {
   return (
     <div className='App'>
       <div>Test</div>
+      <Button>Click me!</Button>
     </div>
   );
 }
