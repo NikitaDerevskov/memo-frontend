c40cf3981dc18d34fc3ccc4ae35fef22c79b93db
Nikita Derevskov <n.derevskov@examus.com>
2022-11-15 08:57:57 +0300

    Api: add AuthService

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,3 +1,15 @@
-export default class ApiService {
-  // TODO install axios
+import axios from 'axios';
+
+export class AuthService {
+  signIn(email: string, password: string) {
+    return axios.post('/api/login', { email, password });
+  }
+
+  register(username: string, email: string, password: string) {
+    return axios.post('/api/register', { username, email, password });
+  }
+
+  getCurrentUserByToken(token: string) {
+    return axios.get('/api/me', { headers: { Authorization: token } });
+  }
 }
