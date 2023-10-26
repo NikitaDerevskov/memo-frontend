a6cf1432c57960d1258c0ce7b732772cfeb3800c
Nikita Derevskov <nikitaderevskov@gmail.com>
2023-10-26 22:56:33 +0200

    footer: show current version

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,6 @@
-/* Because this is my petproject I want to show version */
+/* Because this is my pet project I want to show version */
+import { version } from "../../package.json";
 
 export default function Footer() {
-  return <h2>Footer</h2>;
+  return <h2>Footer {version}</h2>;
 }
